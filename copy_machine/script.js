afcc9886e979bbb70a96ad11d51da40254481d1c
oreo21 <reok99@gmail.com>
2020-11-15 20:40:28 -0600

    added enable/disable button code

--- copy_machine/script.js
+++ copy_machine/script.js
@@ -4,28 +4,60 @@
 // | not implemented yet but it works                                               |
 // |--------------------------------------------------------------------------------|
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+function enableSeparator() {
+	var checkYes = document.getElementById("yes");
+	if (checkYes.checked) {
+		var binA = document.getElementById("sep-a");
+		var binB = document.getElementById("sep-b");
+		var binC = document.getElementById("sep-c");
+		
+		var binArr = [binA, binB, binC];
+		var b;
+		for (b of binArr) {
+			b.disabled = false;
+		}
+	}
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function disableSeparator() {
+	var checkNo = document.getElementById("no");
+	if (checkNo.checked) {
+		var binA = document.getElementById("sep-a");
+		var binB = document.getElementById("sep-b");
+		var binC = document.getElementById("sep-c");
+
+		var binArr = [binA, binB, binC];
+		var b;
+		for (b of binArr) {
+			b.disabled = true;
+			b.checked = false;
+		}
+	}
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
